778c3c55f329ca50b3329a875993460c3bdd33a5
limeter-joaquin <leimeter.joaquin@gmail.com>
2023-01-03 09:55:54 -0300

    CV: migrate Todos.ve into script setup syntax - add sort functionality

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,48 +1,76 @@
-<script lang="ts">
+<script setup lang="ts">
 import { v4 as uuidv4 } from "uuid";
-import { ref } from "vue";
-export default {
-  setup() {
-    const todos = ref([
-      {
-        id: uuidv4(),
-        description: "Go shoppping",
-      },
-      {
-        id: uuidv4(),
-        description: "Call mom",
-      },
-    ]);
+import { reactive, ref, watch } from "vue";
 
-    const inputText = ref("");
+interface Todos {
+  id: string;
+  description: string;
+  priority: number;
+  done: boolean;
+}
 
-    const add = (description: string) => {
-      if (description) {
-        todos.value = [
-          ...todos.value,
-          {
-            id: uuidv4(),
-            description,
-          },
-        ];
-        inputText.value = "";
-      }
-    };
-
-    const remove = (id: string) => {
-      todos.value = todos.value.filter((v) => {
-        return v.id !== id;
-      });
-    };
-
-    return {
-      todos,
-      inputText,
-      add,
-      remove,
-    };
+const todos = ref<Todos[]>([
+  {
+    id: uuidv4(),
+    description: "Go shoppping",
+    priority: 1,
+    done: true,
   },
+  {
+    id: uuidv4(),
+    description: "Call mom",
+    priority: 3,
+    done: false,
+  },
+]);
+
+const newTodo = reactive({
+  textInput: "",
+  priotityInput: 0,
+});
+
+const add = (description: string, priority: number) => {
+  if (newTodo.priotityInput < 0 || newTodo.priotityInput > 3) return;
+  if (description) {
+    todos.value = [
+      ...todos.value,
+      {
+        id: uuidv4(),
+        description,
+        priority,
+        done: false,
+      },
+    ];
+    newTodo.textInput = "";
+  }
 };
+
+const remove = (id: string) => {
+  todos.value = todos.value.filter((v) => {
+    return v.id !== id;
+  });
+};
+
+const sortBy = ref<"priority" | "status">("status");
+watch(sortBy, () => {
+  if (sortBy.value === "priority") {
+    todos.value.sort((a, b) => {
+      if (a["priority"] < b["priority"]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  } else if (sortBy.value === "status") {
+    todos.value.sort((a) => {
+      if (a.done) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+});
 </script>
 
 <template>
@@ -54,20 +82,51 @@ export default {
         v-for="item in todos"
         :key="item.id"
         class="flex justify-end items-center"
+        :class="[{ 'line-through': item.done }]"
       >
         <p class="ml-2">{{ item.description }}</p>
-        <button
-          @click="remove(item.id)"
-          class="rounded-full w-5 h-5 ml-5"
-        ></button>
+        <button @click="remove(item.id)" class="rounded-full w-5 h-5 ml-5">
+          x
+        </button>
       </li>
     </ul>
     <div class="flex justify-around mt-5">
-      <input v-model="inputText" class="text-black outline-0 pl-2" />
-      <button @click="add(inputText)" class="rounded-lg px-2 py-1">
+      <input v-model="newTodo.textInput" class="text-black outline-0 pl-2" />
+
+      <label for="priority">Priority {{ newTodo.priotityInput }}</label>
+      <input
+        type="range"
+        id="priority"
+        name="Priority"
+        min="1"
+        max="4"
+        v-model="newTodo.priotityInput"
+      />
+
+      <button
+        @click="add(newTodo.textInput, newTodo.priotityInput)"
+        class="rounded-lg px-2 py-1"
+      >
         Add item
       </button>
+
+      <label for="priority_sort_selector">Priority</label>
+      <input
+        id="priority_sort_selector"
+        type="radio"
+        v-model="sortBy"
+        value="priority"
+      />
+
+      <label for="status_sort_selector">Status</label>
+      <input
+        id="status_sort_selector"
+        type="radio"
+        v-model="sortBy"
+        value="status"
+      />
     </div>
+    <!-- <pre class="text-xs">{{ JSON.stringify(todos, null, 2) }}</pre> -->
   </div>
 </template>
 
